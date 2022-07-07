a1d32ab5b6e0389fb4fb2fcd1573e6a0d3f2930a
Ways Alqorni <wayslos92@gmail.com>
2022-07-07 10:22:03 +0700

    update

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -2,14 +2,14 @@
 
 @section('isi')
     <div class="container mt-5 mb-5">
-        <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="row row-cols-1 row-cols-md-3">
 
             @foreach ($barangs as $barang)
-                <div class="col">
+                <div class="col mb-5">
                     <div class="card" style="width: 18rem;">
                         <img src="/imageBarang/{{ $barang->gambar }}" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $barang->name }}</h5>
+                        <div class="card-body rounded-bottom" style="background-color: #111827">
+                            <h5 class="card-title text-white">{{ $barang->name }}</h5>
                             <p class="card-text">
                             </p>
                         </div>
